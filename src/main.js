034b43e9b5cf7f205ce9fc8409fb8a92cfca0962
Dianne <dianne.diazm@gmail.com>
2020-11-07 20:24:20 -0300

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCircle, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+library.add( faCircle, faBars, faGithub, faFacebook, faTwitter )
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
